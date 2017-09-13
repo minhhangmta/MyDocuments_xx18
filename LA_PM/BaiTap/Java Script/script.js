de2becd0f12532fcf6b4347b6271643fda8c7919
minhhangmta <minhhang.mta@gmail.com>
2017-09-13 17:46:16 +0700

    add file

---
+++ LA_PM/BaiTap/Java Script/script.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+function myFunction() {
+	alert("Page is loaded");
+}
+
+function hello() {
+	alert("welcome you to my web!")
+	return false;
+}
+
+function changeText(val) {
+	alert("Hello " + val);
+}
+
+function over() {
+	document.write("Mouse Over");
+}
+function out() {
+	document.write("Mouse Out");
+}
