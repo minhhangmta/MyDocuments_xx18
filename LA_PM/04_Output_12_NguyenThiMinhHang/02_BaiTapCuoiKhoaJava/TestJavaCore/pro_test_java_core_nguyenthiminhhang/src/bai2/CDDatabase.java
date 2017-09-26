edd155462d0cfe380e28023814888f42f32ba98f
minhhangmta <minhhang.mta@gmail.com>
2017-09-26 17:28:44 +0700

    upload

---
+++ LA_PM/04_Output_12_NguyenThiMinhHang/02_BaiTapCuoiKhoaJava/TestJavaCore/pro_test_java_core_nguyenthiminhhang/src/bai2/CDDatabase.java
@@ -0,0 +1,183 @@
+/**
+* Copyright(C) 2017 Luvina
+* CDDatabase.java, Sep 26, 2017 minhhang
+*/
+package bai2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Lớp quản lý dữ liệu CD, tác động tới DB
+ * 
+ * @author minhhang
+ */
+public class CDDatabase {
+	private Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+	private String URL = "jdbc:mysql://localhost:3306/cds?";
+	private String USER = "root";
+	private String PASS = "root";
+
+	/**
+	 * Hàm mở connection
+	 */
+	public void getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Hàm thêm mới một CD
+	 * 
+	 * @param cd
+	 *            đối tượng CD
+	 * @return chuỗi thông báo
+	 */
+	public String insertCD(CD cd) {
+		String query = "INSERT INTO cds values(?,?);";
+		String notice = "";
+		try {
+			getConnection();
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, cd.getArtist());
+			preparedStatement.setString(2, cd.getTitle());
+			int excute = preparedStatement.executeUpdate();
+			if (excute > 0) {
+				notice = "Đã thêm!";
+			} else {
+				notice = "Không thể thêm!";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConnection();
+		}
+		return notice;
+	}
+
+	/**
+	 * Hàm xóa một CD
+	 * 
+	 * @param cd
+	 *            đối tượng CD
+	 * @return chuỗi thông báo
+	 */
+	public String removeCD(CD cd) {
+		String notice = "";
+		String query = "DELETE FROM cds WHERE artist = ? and title = ? ;";
+		try {
+			getConnection();
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, cd.getArtist());
+			preparedStatement.setString(2, cd.getTitle());
+			int excute = preparedStatement.executeUpdate();
+			if (excute > 0) {
+				notice = "Đã xóa!";
+			} else {
+				notice = "Không thể xóa!";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			closeConnection();
+		}
+		return notice;
+	}
+
+	/**
+	 * Hàm tìm kiếm theo Title
+	 * 
+	 * @param key
+	 *            từ khóa tìm kiếm
+	 * @return danh sách CD tìm được
+	 */
+	public ArrayList<CD> findByTitle(String key) {
+		ArrayList<CD> listCD = new ArrayList<CD>();
+		String query = "SELECT * FROM cds WHERE title LIKE ? ;";
+		if ("".equals(key)) {
+			return listCD;
+		} else {
+			try {
+				getConnection();
+				key = key.replace("%", "\\%");
+				preparedStatement = connection.prepareStatement(query);
+				preparedStatement.setString(1, "%" + key + "%");
+				ResultSet resultSet = (ResultSet) preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					String ar = resultSet.getString("artist");
+					String ti = resultSet.getString("title");
+					listCD.add(new CD(ar, ti));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				closeConnection();
+			}
+		}
+		return listCD;
+	}
+
+	/**
+	 * Hàm tìm kiếm theo Artist
+	 * 
+	 * @param key
+	 *            từ khóa tìm kiếm
+	 * @return danh sách CD tìm được
+	 */
+	public ArrayList<CD> findByArtist(String key) {
+		ArrayList<CD> listCD = new ArrayList<CD>();
+		String query = "SELECT * FROM cds WHERE artist LIKE ? ;";
+		if ("".equals(key)) {
+			return listCD;
+		} else {
+			try {
+				getConnection();
+				key = key.replace("%", "\\%");
+				preparedStatement = connection.prepareStatement(query);
+				preparedStatement.setString(1, "%" + key + "%");
+				ResultSet resultSet = (ResultSet) preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					String ar = resultSet.getString("artist");
+					String ti = resultSet.getString("title");
+					listCD.add(new CD(ar, ti));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				closeConnection();
+			}
+		}
+		return listCD;
+	}
+
+	/**
+	 * Hàm đóng connection
+	 */
+	public void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ LA_PM/04_Output_12_NguyenThiMinhHang/02_BaiTapCuoiKhoaJava/TestJavaCore/pro_test_java_core_nguyenthiminhhang/src/bai2/DoTest.java
@@ -0,0 +1,49 @@
+/**
+* Copyright(C) 2017 Luvina
+* DoTest.java, Sep 26, 2017 minhhang
+*/
+package bai2;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+/**
+ * Lớp test các method trong CDDatabase
+ * 
+ * @author minhhang
+ */
+public class DoTest {
+	/**
+	 * Hàm main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CDDatabase cdDatabase = new CDDatabase();
+		CD cd1 = new CD("Charlie", "See you again");
+		CD cd2 = new CD("Taylor", "Style");
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		while (true) {
+			System.out.println("Hay nhap vao so (1-4) de chon: ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println(cdDatabase.insertCD(cd1));
+				return;
+			case 2:
+				System.out.println(cdDatabase.removeCD(cd2));
+				return;
+			case 3:
+				System.out.println(cdDatabase.findByTitle("%").toString());
+				return;
+			case 4:
+				System.out.println(cdDatabase.findByArtist("ha").toString());
+				return;
+			default:
+				System.out.println("Hãy nhập hàm cần test là từ 1 đến 4");
+				break;
+			}
+		}
+
+	}
+}
